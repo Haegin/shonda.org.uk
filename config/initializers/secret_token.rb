5cc1595db142bccaef0a13669e51a89330ef9dc7
Harry Mills <harry@haeg.in>
2013-12-27 18:53:37 +0000

    Initial commit - sets up engine

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shonda::Application.config.secret_token = 'a7beb0a599947225eb84c96c21f01b23c823d7bf00d7cfba907c1f143f3bbcc7c62ec04f7c8fa1041a8f1320259423791ba4231c4af8360a19347a2439f8ee42'
