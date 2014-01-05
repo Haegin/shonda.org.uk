0111ea969e69590234c7b8066a7817f2dea8709f
Harry Mills <harry@haeg.in>
2014-01-05 22:46:39 +0000

    Try to fix asset problems

--- config/application.rb
+++ config/application.rb
@@ -23,15 +23,6 @@ module Shonda
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
-    config.autoload_paths += %W(#{config.root}/lib)
-
-    module Locomotive::Liquid::Drops
-      class Page
-        def summary
-          @_source.templatized? ? @context['entry']._summary.singularize : @_source.summary
-        end
-      end
-    end
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
