0de0d2b8aecdcd1c249c922caf8b4f8507a84901
Harry Mills <harry@haeg.in>
2014-01-23 21:58:06 +0000

    Change PID file in unicorn_init.rb

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,9 +12,9 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deploy/shonda.org.uk/current
-PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; chruby-exec 1.9.3 -- bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+APP_ROOT=/home/deploy/shonda.org.uk
+PID=$APP_ROOT/shared/pids/unicorn.pid
+CMD="cd $APP_ROOT/current; chruby-exec 1.9.3 -- bundle exec unicorn -D -c $APP_ROOT/current/config/unicorn.rb -E production"
 AS_USER=deploy
 set -u
 
